f1220ec75632139b6d2aefc4a8f5ea379f312ac8
rgh <richardharding305@gmail.com>
2023-07-23 22:57:14 -0500

    setup for pdas react project

---
+++ pdas/src/index.js
@@ -0,0 +1,9 @@
+import react from 'react';
+import reactDOM from 'react-dom'
+import App from './App';
+
+const el = document.getElementById('root');
+const root = reactDOM.createRoot(el);
+
+
+root.render(<App />);

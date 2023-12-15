9e53cc3483ab19967d7e408ec12c419711342001
rgh <richardharding305@gmail.com>
2023-12-15 00:34:41 -0600

    pdas App Component updated with new Card Component

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,8 +1,12 @@
+import Card from './Card';
+
 function App() {
 
   return (
   <div>
-    App
+    <Card name={'Siri'} handle={'@siri44'} theme={'Siri theme'} pic={'siriPic'} />
+    <Card name={'Alexa'} handle={'@alexa99'} theme={'Alexas saying'} />
+    <Card name={'Cortana'} handle={'@cortana12'} theme={'Cortana saying'}  />
   </div>
   );
 }

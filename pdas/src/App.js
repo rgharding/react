ba446f445b377e9f435062362afc59de38ce32b6
rgh <richardharding305@gmail.com>
2024-01-01 00:18:02 -0600

    pdas modified App added alt tags

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import Card from './Card';
 import siriPic from './images/siri.png';
 import alexaPic from './images/alexa.png';
@@ -9,9 +10,9 @@ function App() {
   <div>
     <div>Personal Digital Assistant</div>
 
-    <Card title='Siri' handle='@siri44' theme={'Siri theme'} pic={siriPic} alt="logoPic"   color='red' />
-    <Card title='Alexa' handle='@alexa99' theme={'Alexas saying'} pic={alexaPic} alt="logoPic"/>
-    <Card title='Cortana' handle='@cortana12' theme={'Cortana saying'} pic={cortanaPic} alt="logoPic"/>
+    <Card title='Siri' handle='@siri44' theme='Siri theme' image={siriPic} alt="logoPic"/>
+    <Card title='Alexa' handle='@alexa99' theme='Alexas saying' image={alexaPic} alt="logoPic" />
+    <Card title='Cortana' handle='@cortana12' theme='Cortana saying' image={cortanaPic} alt="logoPic" />
   </div>
   );
 }

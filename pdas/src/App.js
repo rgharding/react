76de06c8d13de6cc30d125abcf1a6d5721bd40b1
rgh <richardharding305@gmail.com>
2024-01-07 00:02:09 -0600

    pdas App added Bulma Styling

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -8,12 +8,30 @@ function App() {
 
   return (
   <div>
-    <div>Personal Digital Assistant</div>
 
-    <Card title='Siri' handle='@siri44' theme='Siri theme' image={siriPic} alt="logoPic"/>
-    <Card title='Alexa' handle='@alexa99' theme='Alexas saying' image={alexaPic} alt="logoPic" />
-    <Card title='Cortana' handle='@cortana12' theme='Cortana saying' image={cortanaPic} alt="logoPic" />
-  </div>
+    <section className="hero is-link">
+      <div className='hero-body'>
+      <p className="title">Personal Digital Assistant</p>
+        </div>
+    </section>
+
+    <div className='container'>
+      <section className="section">
+        <div className='columns'>
+          <div className='column is-4'>
+            <Card title='Siri' handle='@siri44' theme='Siri theme' image={siriPic} alt="logoPic"/>
+      </div>
+      <div className='column is-4'>
+        <Card title='Alexa' handle='@alexa99' theme='Alexas saying' image={alexaPic} alt="logoPic" />
+      </div>
+      <div className='column is-4'>
+        <Card title='Cortana' handle='@cortana12' theme='Cortana saying' image={cortanaPic} alt="logoPic" />
+      </div>
+      </div>
+      </section>
+
+    </div>
+</div>
   );
 }
 

232def34ec30404a6657f9a385ceb277884fdb57
rgh <richardharding305@gmail.com>
2024-01-10 00:09:33 -0600

    AnimalShow App created and wired up count button

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import AnimalShow from "./AnimalShow";
 
 function App() {
 
+  const [ count, setCount ] = useState(0);
+
+  const handleClick = ((event) => {
+    event.preventDefault();
+    setCount(count + 1);
+  });
+
   return (
-    <div>Animal Show</div>
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      Count: {count}
+    </div>
   )
 };
 

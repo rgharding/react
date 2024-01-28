f8cccc6fe3c11041c7136bb1242f5378cb9f2289
rgh <richardharding305@gmail.com>
2024-01-27 21:40:24 -0600

    index logic added no xml/css

--- animals/src/index.js
+++ animals/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom/client'
-import App from './App';
+import ReactDom from 'react-dom/client';
+import App from './App'
+
 
 const el = document.getElementById('root');
 const root = ReactDom.createRoot(el);
 
-root.render(
-  <App />
-)
+root.render(<App />)

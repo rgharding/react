f9647b35de941f6a068070e0e67d9580c625f99b
rgh <richardharding305@gmail.com>
2024-05-27 22:56:55 -0500

    BookEdit component --- no data persistence

--- books/src/components/BookEdit.js
+++ books/src/components/BookEdit.js
@@ -1,12 +1,28 @@
-// Needs State
+import { useState } from 'react';
 
-function BookEdit() {
+function BookEdit(props) {
+
+  const {book, onSubmit } = props;
+
+  const [ title, setTitle ] = useState(book.title)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(book.id, title);
+  }
+
+  const handleChange = (event) => {
+    setTitle(event.target.value)
+  }
 
   return (
-    <div>
-      BookEdit
-    </div>
-  );
+    
+      <form onSubmit={handleSubmit} className="book-edit">
+        <label>Title</label>
+        <input className='input'  value={title} onChange={handleChange}/>
+        <button className="button is-primary"> Save </button>
+      </form>
+      );
 };
 
 

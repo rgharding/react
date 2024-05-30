3455cfa058d374004490e7239651683c92d82278
rgh <richardharding305@gmail.com>
2024-05-29 22:59:58 -0500

    BookShow component --- no data persistence

--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -1,10 +1,41 @@
-// Needs State
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+ 
+function BookShow(props) {
 
-function BookShow() {
+  const { book, onDelete, onEdit } = props;
+
+  const [ showEdit, setShowEdit ] = useState(false);
+ 
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+
+  let content = <h3>{book.title}</h3>
+
+  const handleSubmit = (id, newTitle) => {
+    onEdit(id, newTitle);
+    setShowEdit(false);
+  }
+
+  if (showEdit) {
+    content = <BookEdit book={book} onSubmit={handleSubmit} />
+  }
 
   return (
-    <div>
-      BookShow
+    <div className="book-show" >
+      <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt='random pic' />
+      <div>{content}</div>
+      
+      <div className='actions'>
+       <button className='delete' onClick={handleDeleteClick}>Delete</button>
+       <button className='edit' onClick={handleEditClick}>Edit</button>
+      </div>
     </div>
   );
 };

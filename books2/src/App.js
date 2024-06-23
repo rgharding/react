7ff38d4d9ce220169c1614d5a3abe9604f0f564c
rgh <richardharding305@gmail.com>
2024-06-23 17:55:53 -0500

    App component wired up to BookCreate component. Receiving title event.

--- books2/src/App.js
+++ books2/src/App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 99999)
+}
+
 function App() {
+
+  const [ books, setBooks ] = useState([]);
+
+  const onBookCreate = (title) => {
+    let newBook = {title: title, id: getRandomNumber()}
+    setBooks([...books, newBook]);
+}
+
 
   return (
     <div>
-      App
+      <BookCreate onSubmit={onBookCreate} />
     </div>
   );
 };
+
 
 
 export default App;

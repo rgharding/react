3086127934464f731d3f7f04fc790af6f20a86b9
rgh <richardharding305@gmail.com>
2024-06-24 17:57:29 -0500

    BookCreate component wired to App. Receiving props and sending title event.

--- books2/src/components/BookCreate.js
+++ books2/src/components/BookCreate.js
@@ -1,8 +1,31 @@
-function BookCreate() {
+import { useState } from 'react';
+
+function BookCreate(props) {
+
+  const { onSubmit } = props;
+
+  const [ title, setTitle ] = useState('');
+
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(title);
+    setTitle('')
+  };
+  
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  }
+
 
   return (
     <div>
-      BookCreate
+      <form onSubmit={handleSubmit}>
+        <label>Add Book</label>
+        <input onChange={handleChange} value={title}/>
+        <button>Save</button>
+      </form>
     </div>
   );
 };

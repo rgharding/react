682cc595445629b5ee10c8b6d319ec74c7df6429
rgh <richardharding305@gmail.com>
2024-08-30 02:36:16 -0500

    BookEdit Component Completed

--- books2/src/components/BookEdit.js
+++ books2/src/components/BookEdit.js
@@ -2,14 +2,29 @@ import { useState } from 'react';
 
 function BookEdit(props) {
 
-  const { book } = props
+  const { book, onSubmit } = props
 
   const [ newTitle, setNewTitle ] = useState(book.title); 
 
+  const hanldeSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(newTitle, book.id);
+    
+  }
+
+
+  const handleChange = (event) => {
+    setNewTitle(event.target.value);
+
+  }
+
+  
   return (
-    <div>
-      <form>
-        <input value={newTitle}/>
+    <div className='book-edit'>
+      <form onSubmit={hanldeSubmit}>
+        <label>Title</label>
+        <input className='input' onChange={handleChange} value={newTitle}/>
+        <button className='button is-primary'>Save</button>
       </form>
     </div>
   );

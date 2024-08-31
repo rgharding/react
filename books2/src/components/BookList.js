2651893d647f5cd20276c863f78dec5c85d9c4a9
rgh <richardharding305@gmail.com>
2024-08-31 02:37:14 -0500

    BookList Component Complete

--- books2/src/components/BookList.js
+++ books2/src/components/BookList.js
@@ -4,10 +4,10 @@ import BookShow from './BookShow'
 
 function BookList(props) {
 
-  const { books, onDelete } = props
+  const { books, onDelete, onEdit } = props
   
   const renderedBooks = books.map(book => {
-    return <BookShow book={book} key={book.id} onDelete={onDelete} />
+    return <BookShow book={book} key={book.id} onDelete={onDelete} onEdit={onEdit} />
   })
  
   return (

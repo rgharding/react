e1a75dd04c64bf949b02397d767ed076656a6ad3
rgh <richardharding305@gmail.com>
2024-09-03 18:48:04 -0500

    index file: added comment about css class names for all components

---
+++ books_with_persistence/src/index.js
@@ -0,0 +1,37 @@
+/*
+css-->
+
+index: 
+import css
+
+
+App: 
+div --> app
+h3 --> title
+
+
+
+BookCreate:
+div -->  book-create
+input --> input
+button --> button
+
+
+BookEdit: 
+div --> book-edit
+input --> input
+button --> button is-primary
+
+
+
+BookShow: 
+div --> book-show
+div --> actions
+button --> delete
+button --> edit
+
+
+BookList:
+div --> book-list
+
+*/

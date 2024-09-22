1581b3637dba174cbdad807e9a5023dece586502
rgh <richardharding305@gmail.com>
2024-09-22 08:20:57 -0500

    Imported useState, added form with input and buttons (not wired up) and css

--- books_with_persistence/src/components/BookCreate.js
+++ books_with_persistence/src/components/BookCreate.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 function BookCreate() {
 
+  const [ title, setTitle ] = useState('');
+
   return (
-    <div>
-      BookCreate
+    
+    <div className='book-create'>
+      <form>
+        <input className='input' />
+        <button className='button' >Save</button>
+      </form>
     </div>
-  );
+  )
 };
 
 export default BookCreate;

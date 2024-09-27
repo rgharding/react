0c41683fe89bb2d8413afc5ebace43cf3adf5307
rgh <richardharding305@gmail.com>
2024-09-27 00:46:47 -0500

    App component wired up to BookCreate component. onCreate Function made with getRamdomId Func as well. BookObject will be created

--- books_with_persistence/src/App.js
+++ books_with_persistence/src/App.js
@@ -1,14 +1,30 @@
+import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+
+
+const getRandomId = () => {
+  return Math.floor(Math.random() * 99999);
+}
+
+
 
 function App() {
 
+  const [ books, setBooks ] = useState([]);
 
+  const onBookCreate = (title) => {
+    let newBook = { title: title, id: getRandomId() }
+    let updatedBooks = [newBook, ...books];
+    setBooks(updatedBooks)
+  }
+
+  
   return (
     <div>
-      <BookCreate />
+      <BookCreate onCreate={onBookCreate} />
     </div>
   );
-}
+};
 
 
 export default App;

6980e52fa3ad97f0a82dd737759757205a24ec5c
rgh <richardharding305@gmail.com>
2024-09-28 00:48:18 -0500

    BookCreate component recieves prop from App's onCreate function. Wired up and send book title to App. Completed

--- books_with_persistence/src/components/BookCreate.js
+++ books_with_persistence/src/components/BookCreate.js
@@ -1,16 +1,30 @@
 import { useState } from "react";
 
-function BookCreate() {
+function BookCreate(props) {
+
+  const { onCreate } = props
 
   const [ title, setTitle ] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle('')
+  }
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  }
 
   return (
     
     <div className='book-create'>
-      <form>
-        <input className='input' />
+      
+      <form onSubmit={handleSubmit}>
+        <input value={title} onChange={handleChange} className='input' />
         <button className='button' >Save</button>
       </form>
+
     </div>
   )
 };

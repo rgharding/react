73e8e1602a0f7f2241fc36dc46c0748e1f54ebd9
rgh <richardharding305@gmail.com>
2024-11-08 22:29:54 -0600

    Component Completed NO Persistence

--- books_with_persistence/src/components/BookEdit.js
+++ books_with_persistence/src/components/BookEdit.js
@@ -1,8 +1,31 @@
-function BookEdit() {
+import { useState } from "react";
+
+function BookEdit(props) {
+
+  const { handleEditSubmit, book} = props
+
+  const [newTitle, setNewTitle ] = useState(book.title) 
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleEditSubmit(newTitle, book.id)
+    setNewTitle('');
+  }
+
+
+  const handleChange = (event) => {
+    setNewTitle(event.target.value);
+   }
 
   return (
     <div>
-      BookEdit
+      <label>Edit Title</label>
+      <form onSubmit={handleSubmit} className="book-edit">
+        <input value={newTitle} onChange={handleChange} />
+        <button className='button is-primary' >Save</button>
+      </form>
+      
     </div>
   );
 };

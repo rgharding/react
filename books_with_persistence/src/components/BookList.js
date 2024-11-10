0362125402db055b7c326403b3926e366758706f
rgh <richardharding305@gmail.com>
2024-11-09 22:30:20 -0600

    Component Completed NO Persistence

--- books_with_persistence/src/components/BookList.js
+++ books_with_persistence/src/components/BookList.js
@@ -2,11 +2,11 @@ import BookShow from './BookShow';
 
 function BookList(props) {
 
-  const { books } = props
+  const { books, onDelete, onEdit } = props
 
   const renderedBook = books.map(book => {
-    return <BookShow  book={book} key={book.id} />
-  })
+    return <BookShow  book={book} key={book.id}  onDelete={onDelete} onEdit={onEdit}/>
+  });
 
   return (
     <div className='book-list' >

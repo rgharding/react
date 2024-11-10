8f2e94736f933b0616e43f4f64e7f1dae203221d
rgh <richardharding305@gmail.com>
2024-11-10 03:39:28 -0600

    BookShow linked with bookEdit component. Logic for showing BookEdit added

--- books2/src/components/BookShow.js
+++ books2/src/components/BookShow.js
@@ -1,8 +1,35 @@
-function BookShow() {
+import BookEdit from './BookEdit';
+import { useState } from 'react';
+
+function BookShow(props) {
+
+
+const { book, onDelete } = props;
+
+const [showEdit, setShowEdit ] = useState(false);
+
+const handleDeleteClick = () => {
+  onDelete(book.id);
+}
+
+const handleEditClick = () => {
+  setShowEdit(!showEdit)
+}
+
+
+let content = <h3>{book.title}</h3>
+if (showEdit) {
+  content = <BookEdit boook={book} />
+}
 
   return (
-    <div>
-      BookShow
+    <div className='book-show'>
+      <div>{content}</div>
+      
+      <div className='actions'>
+        <button onClick={handleDeleteClick}>Delete</button>
+        <button onClick={handleEditClick}>Edit</button>
+      </div>
     </div>
   );
 };

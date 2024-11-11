6a27a6d9b12b985d8bd1319bc3b35660467cc5aa
rgh <richardharding305@gmail.com>
2024-11-10 22:30:53 -0600

    Component Completed NO Persistence

--- books_with_persistence/src/components/BookShow.js
+++ books_with_persistence/src/components/BookShow.js
@@ -1,11 +1,49 @@
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
+
 function BookShow(props) {
 
-  const { book } = props;
+  const { book, onDelete, onEdit } = props;
+
+  const [ edit, setBookEdit ] = useState(false);
+
+  
+  const handleEditClick = (event) => {
+    setBookEdit(!edit);
+  }
 
 
+  const handleDeleteClick = () => {
+    onDelete(book.id)
+  }
+
+
+  const handleSubmit = (title, id) => {
+    onEdit(title, id);
+    setBookEdit(false);
+  }
+  
+
+  let content = <div>{book.title}</div>
+  
+  if (edit) {
+    content = <BookEdit book={book} handleEditSubmit={handleSubmit} />
+  }
+  
+  
   return (
-    <div>
-      {book.title}
+    <div className='book-show'>
+      <div>
+        <img src={`https://picsum.photos/seed/${book.id}/300/200`} />
+      </div>
+      <div className='actions'>
+        <button onClick={handleEditClick} className='edit'>edit</button>
+        <button onClick={handleDeleteClick} className='delete'>delete</button>
+      </div>
+
+      {content}
+      
     </div>
   );
 };

03e13f4f76093d87814d3666c91e31004255bbb7
rgh <richardharding305@gmail.com>
2024-11-19 22:31:28 -0600

    basic index setup

--- books_with_persistence/src/index.js
+++ books_with_persistence/src/index.js
@@ -1,3 +1,19 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import App from './App'
+
+
+const el = document.getElementById('root');
+const root = ReactDom.createRoot(el);
+
+
+root.render(<App />);
+
+
+
+
+
+
 /*
 css-->
 
